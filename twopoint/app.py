66e51f0d5e202ee239636c529dad865ca66490ee
Edwar Alfredo Villamizar Rincon <1094269584@colmayor.loc>
2022-08-04 16:15:23 -0500

    primer panel

--- twopoint/app.py
+++ twopoint/app.py
@@ -1,8 +1,8 @@
 while True:
 
     try:
-        nv = int(input('Ingrese numero de vacas: '))
-        if nv >= 3 and nv <= 50:
+        numeroVacas = int(input('Ingrese numero de vacas: '))
+        if numeroVacas >= 3 and numeroVacas <= 50:
             break
         else:        
             print("El numero tiene que estar entre 3 y 50", end= "\n")
@@ -11,50 +11,47 @@ while True:
         print("Error Solo Numeros", end= "\n")
         print(end= "\n")
 
-    
-
 while True:
 
     lst=list()
     mtr=list()
 
     for i in range(7):
-        for j in range(0,nv):
+        for j in range(0,numeroVacas):
 
             while True:
 
-
-
                 try:
-                    nv = int(input('Ingrese numero de vacas: '))
-                    if nv >= 3 and nv <= 50:
+                    litroLeche = float(input('Litros de leche, dia '+str(i+1)+',vaca '+str(j+1)+": "))
+                    if litroLeche >= 0.0 and litroLeche <= 11.9:
                         break
                     else:        
-                        print("El numero tiene que estar entre 3 y 50", end= "\n")
+                        print("El numero tiene que estar entre 0.0 y 11.9", end= "\n")
                         print(end= "\n")
                 except ValueError:
                     print("Error Solo Numeros", end= "\n")
                     print(end= "\n")
 
-            lst.append(input("Dia: "+str(i+1)+", Vaca: "+str(j+1)+" "))
+            lst.append(litroLeche)
 
         mtr.append(lst)
         lst=list()
+
     break
-
- 
-
-
 
 while True:
 
-    print ("1. Opcion 1")
-    print ("2. Opcion 2")
-    print ("3. Opcion 3")
+    print ("1. Produccion Total")
+    print ("2. mayor menor")
+    print ("3. numero de")
     print ("4. Salir")
-     
-    print ("Elige una opcion")
- 
+
     option = input('Ingrese Opcion')
+
+    if option == "1": break
+
+    if option == "2": break
+
+    if option == "3": break
 
     if option == "4": break
