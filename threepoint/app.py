6c4e89b3f5480cb1e6c21c47c2e5290ebb841f7c
EdwarVillamizar <villamizar.edwar@gmail.com>
2022-08-05 21:54:27 -0500

    Full

--- threepoint/app.py
+++ threepoint/app.py
@@ -6,7 +6,7 @@ def database():
     conexion=sqlite3.connect("arcade.db")
 
     try:
-        conexion.execute("""create table gamer (
+        conexion.execute("""Create TABLE if not exists gamer (
                                 id integer primary key autoincrement,
                                 name text,
                                 score text
@@ -17,28 +17,28 @@ def database():
 
     except sqlite3.OperationalError:
         pass
-        #print("La tabla gamer ya existe")   
 
     try:
-        conexion.execute("""create table palabras(
+        conexion.execute("""Create TABLE if not exists palabras(
                                 id integer primary key autoincrement,
-                                name text,
+                                name text
                             )""")
         conexion.commit()  
 
     except sqlite3.OperationalError:
         pass
-        #print("La tabla gamer ya existe")  
 
-    palabras = ['hola','perro','gato']
+    palabras = ['lobo','perro','gato','zorro','loro']
     cursor = conexion.cursor()
-    cursor.executemany('INSERT INTO palabras VALUES(?,?,?);',palabras)
+    for item in palabras:
+        cursor.execute("INSERT OR IGNORE INTO palabras(name) values (?)", (item,))
+        conexion.commit()
     conexion.execute("INSERT OR IGNORE INTO gamer(id,name,score) values (?,?,?)", (1,"Player 1",0))
     conexion.commit() 
 
     conexion.close()
 
-def opcionUno(id):
+def opcionUno(id,life):
 
     print(end= "\n")
 
@@ -46,17 +46,14 @@ def opcionUno(id):
     print ("2. Ahorcado.")
     print ("3. Astucia Naval.")
 
-    print ("4. Regresar")
-
     print(end= "\n")
     option = input('Ingrese Opcion: ')
     print(end= "\n")
 
     if option == "1": primerJuego(id)
-    if option == "2": segundoJuego(id)
-    if option == "3": pass
+    if option == "2": segundoJuego(id,life)
+    if option == "3": tercerJuego()
     
-
 def opcionDos():
 
     print(end= "\n")
@@ -108,7 +105,12 @@ def opcionTres():
     
     return option
 
-def opcionCuatro(): pass
+def opcionCuatro(): 
+
+    print(end= "\n")
+    option = int(input('Ingrese numero de vidas: '))
+    print(end= "\n")
+    return option
 
 def opcionCinco():
 
@@ -130,58 +132,58 @@ def opcionCinco():
     print(end= "\n")
     conexion.close()
         
-
 def primerJuego(id):
     
-    aleatorio = random.randrange(0, 3)
-    elijePc = ""
+    print(end= "\n")
     print("1. Piedra")
     print("2. Papel")
     print("3. Tijera")
+    print(end= "\n")
+    
     opcion = int(input("Que elijes: "))
+    pc = random.randrange(0, 3)
 
     if opcion == 1:
-        elijeUsuario = "piedra"
+        jugador = "piedra"
     elif opcion == 2:
-        elijeUsuario = "papel"
+        jugador = "papel"
     elif opcion == 3:
-        elijeUsuario = "tijera"
-    print("Tu elijes: ", elijeUsuario)
+        jugador = "tijera"
+    print("Tu elijes: ", jugador)
 
-    if aleatorio == 0:
-        elijePc = "piedra"
-    elif aleatorio == 1:
-        elijePc = "papel"
-    elif aleatorio == 2:
-        elijePc = "tijera"
-    print("PC elijio: ", elijePc)
-    print("...")
+    if pc == 0:
+        pc = "piedra"
+    elif pc == 1:
+        pc = "papel"
+    elif pc == 2:
+        pc = "tijera"
+    print("PC: ", pc)
 
-    if elijePc == "piedra" and elijeUsuario == "papel":
-        print("Ganaste, Papel envulve Piedra")
+    if pc == "piedra" and jugador == "papel":
+        print("Ganaste, Papel gana Piedra")
         score = 1
 
-    elif elijePc == "papel" and elijeUsuario == "tijera":
-        print("Ganaste, Tijera corta Papel")
+    elif pc == "papel" and jugador == "tijera":
+        print("Ganaste, Tijera gana Papel")
         score = 1
 
-    elif elijePc == "tijera" and elijeUsuario == "piedra":
-        print("Ganaste, Piedra pisa Tijera")
+    elif pc == "tijera" and jugador == "piedra":
+        print("Ganaste, Piedra gana Tijera")
         score = 1
 
-    if elijePc == "papel" and elijeUsuario == "piedra":
-        print("Perdiste, Papel envulve Piedra")
+    if pc == "papel" and jugador == "piedra":
+        print("Perdiste, Papel gana Piedra")
         score = 0
 
-    elif elijePc == "tijera" and elijeUsuario == "papel":
-        print("Perdiste, Tijera corta papel")
+    elif pc == "tijera" and jugador == "papel":
+        print("Perdiste, Tijera gana papel")
         score = 0
 
-    elif elijePc == "piedra" and elijeUsuario == "tijera":
-        print("Perdiste, Piedra pisa tijera")
+    elif pc == "piedra" and jugador == "tijera":
+        print("Perdiste, Piedra gana tijera")
         score = 0
 
-    elif elijePc == elijeUsuario:
+    elif pc == jugador:
         print("Empate")
         score = 0
     
@@ -190,69 +192,87 @@ def primerJuego(id):
     conexion.commit()
     conexion.close()
 
-def segundoJuego(id):
+def segundoJuego(id,life):
+
+    conexion=sqlite3.connect("arcade.db")
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM palabras")
     
-    palabra_secreta = "hola"
-    vidas = 5
+    rows = cursor.fetchall()
+    lst = list()
+
+    for row in rows:
+
+        lst.append(row[1])
+
+    conexion.close()
+    
+    palabra = random.choice(lst)
  
-    lista_letras_adivinadas = []
+    lst = list()
     
-    ## Imprimimos la palabra sin letras
-    print('_' * len(palabra_secreta))
+    print('_' * len(palabra))
     
     while True:
     
         while True:
-            letra_adivinada = input("Adivina una letra: ")
-            if(len(letra_adivinada)!=1 and letra_adivinada.isnumeric()):
-                print("Eso no es una letra intenta con una sola letra")
+            option = input("Adivina una letra: ")
+            if(len(option)!=1 and option.isnumeric()):
+                print("Solo una letra")
             else:
-                if letra_adivinada.lower() in lista_letras_adivinadas:
-                    print("Ya habias intentado con esa letra intenta con otra por favor")
+                if option.lower() in lst:
+                    print("Con esa letra ya lo habias intentado")
                 else:
-                    lista_letras_adivinadas.append(letra_adivinada)
+                    lst.append(option)
     
-                    if letra_adivinada.lower() in palabra_secreta:
+                    if option.lower() in palabra:
                         print("Felicidades adivinaste una letra")
+
+                        conexion=sqlite3.connect("arcade.db")
+                        conexion.execute("update gamer set score = score + ? where id = ?", (1,id))
+                        conexion.commit()
+                        conexion.close()
+
                         break
                     else:
-                        vidas = vidas-1
+                        life = life-1
                         print("Te haz equivocado y perdido una vida")
-                        print("Te quedan " + str(vidas) + " vidas")
+                        print("Te quedan " + str(life) + " vida")
                         break
     
-        if vidas==0:
-            print("Haz perdido la palabra secreta era: "+ palabra_secreta)
+        if life==0:
+            print("Perdiste, la palabra secreta era: "+ palabra)
             break
     
+        actual = ""
     
-        estatus_actual = ""
-    
-        letras_faltantes = 0
-        for letra in palabra_secreta:
+        letras = 0
+        for letra in palabra:
     
     
-            if letra in lista_letras_adivinadas:
-                estatus_actual = estatus_actual + letra
+            if letra in lst:
+                actual = actual + letra
     
             else:
-                estatus_actual = estatus_actual + "_"
-                letras_faltantes = letras_faltantes + 1
+                actual = actual + "_"
+                letras = letras + 1
     
-        ## Imprimir palabra con algunas letras
-        print(estatus_actual)
+        print(actual)
     
     
-        if letras_faltantes == 0:
-            print("Felicidades haz ganado")
-            print("La palabra secreta es: " + palabra_secreta)
+        if letras == 0:
+            print("Ganaste")
+            print("La palabra secreta es: " + palabra)
             break
+
+def tercerJuego(): pass
 
 if __name__ == "__main__":
 
     database()
     
     id = opcionTres()
+    life = 3
 
     while True:
 
@@ -271,10 +291,10 @@ if __name__ == "__main__":
         option = input('Ingrese Opcion: ')
         print(end= "\n")
 
-        if option == "1": opcionUno(id)
+        if option == "1": opcionUno(id,life)
         if option == "2": opcionDos()
         if option == "3": id = opcionTres()
-        if option == "4": pass
+        if option == "4": life = opcionCuatro()
         if option == "5": opcionCinco()
         if option == "6": break
 
